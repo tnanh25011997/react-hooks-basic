fc0c726a68be47898e2f5cb8444a53317fe6525a
unknown <tnanh25011997@gmail.com>
2020-11-26 21:26:35 +0700

    get list API and Search

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.scss';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import queryString from 'query-string';
 import TodoList from './components/TodoList';
 import TodoForm from './components/TodoForm';
+import PostList from './components/PostList';
+import Pagination from './components/Pagination';
+import PostFilterForm from './components/PostFilterForm';
 
 function App() {
 
@@ -10,6 +14,55 @@ function App() {
     { id: 2, title: 'I Love 2' },
     { id: 3, title: 'I Love 3' }
   ]);
+
+  const [postList, setPostList] = useState([]);
+
+  const [pagination, setPagination] = useState({
+    _page: 1,
+    _limit: 10,
+    _totalRows: 1,
+  });
+
+  const [filters, setFilters] = useState({
+    _limit: 10,
+    _page: 1,
+    title_like: '',
+  });
+
+  useEffect(() => {
+    async function fetchPostList() {
+
+      try {
+        // _limit=10&_page=1
+        const paramString = queryString.stringify(filters);
+
+        const requestUrl = `http://js-post-api.herokuapp.com/api/posts?${paramString}`;
+        const response = await fetch(requestUrl);
+        const responseJSON = await response.json();
+        console.log({ responseJSON });
+
+        const { data, pagination } = responseJSON;
+        setPostList(data);
+        setPagination(pagination);
+      } catch (error) {
+        console.log('Fail to fetch post list: ', error.message);
+      }
+    }
+
+    fetchPostList();
+
+  }, [filters]);
+
+
+
+
+  function handlePageChange(newPage) {
+    console.log(newPage);
+    setFilters({
+      ...filters,
+      _page: newPage,
+    })
+  }
 
   function handleTodoClick(todo) {
     const index = todoList.findIndex(x => x.id === todo.id);
@@ -34,11 +87,27 @@ function App() {
     setTodoList(newTodoList);
   }
 
+
+  function handleFilterChange(newFilters) {
+    setFilters({
+      ...filters,
+      _page: 1,
+      title_like: newFilters.searchTerm,
+    })
+  }
+
   return (
     <div className="app">
       <h1>From VN With Love</h1>
-      <TodoForm onSubmit={handleToDoFormSubmit} />
-      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
+      {/* <TodoForm onSubmit={handleToDoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} /> */}
+
+      <PostFilterForm onSubmit={handleFilterChange} />
+      <PostList posts={postList} />
+      <Pagination
+        pagination={pagination}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
