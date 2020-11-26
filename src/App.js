a24c2df593630d8bd3645881e0865c1772309b78
unknown <tnanh25011997@gmail.com>
2020-11-26 16:48:25 +0700

    Todo List

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import './App.scss';
-import ColorBox from './components/ColorBox';
+import React, { useState } from "react";
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
+
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: 'I Love 1' },
+    { id: 2, title: 'I Love 2' },
+    { id: 3, title: 'I Love 3' }
+  ]);
+
+  function handleTodoClick(todo) {
+    const index = todoList.findIndex(x => x.id === todo.id);
+    if (index < 0) return;
+
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+
+  }
+
+  function handleToDoFormSubmit(formValues) {
+    console.log(formValues);
+
+    const newTodo = {
+      id: todoList.length + 1,
+      ...formValues,
+    }
+
+    const newTodoList = [...todoList];
+    newTodoList.push(newTodo);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
       <h1>From VN With Love</h1>
-
-      <ColorBox />
+      <TodoForm onSubmit={handleToDoFormSubmit} />
+      <TodoList todos={todoList} onTodoClick={handleTodoClick} />
     </div>
   );
 }
